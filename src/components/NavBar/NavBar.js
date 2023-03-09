5da7073dd42b2654ac798ba5b09c4f7b41abca1a
RubsRafa <rubiarafaela2000@gmail.com>
2023-03-09 18:33:27 -0300

    feat: logout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,36 @@
-import { StyledNavBar } from "./styled";
-import { VscChevronDown } from "react-icons/vsc";
+import { Bar, Logo, LogoutFocus } from "./styled";
+import { VscChevronDown, VscChevronUp } from 'react-icons/vsc';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function NavBar() {
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
+
     return (
-
-        <StyledNavBar>
-            <div><h1>linker</h1></div>
+        <Bar>
+            <Logo>linkr</Logo>
             <div>
-                <ion-icon>
-                    <VscChevronDown />
-                </ion-icon>
-                <img src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image04_grd.png" alt="imagem" />
+                {!open && <VscChevronUp
+                    size={30}
+                    style={{ position: 'absolute', top: 22, right: 85, zIndex: 3 }}
+                    color={"#ffffff"}
+                    onClick={() => setOpen(true)}
+                />}
+                {open && <VscChevronDown
+                    size={30}
+                    style={{ position: 'absolute', top: 22, right: 85, zIndex: 3 }}
+                    color={"#ffffff"}
+                    onClick={() => setOpen(false)}
+                />}
+                <img onClick={() => setOpen(!open)} src="https://i.kym-cdn.com/photos/images/newsfeed/002/304/580/313.jpg" alt="" />
+                {open && <h2
+                    onClick={() => {
+                        localStorage.clear()
+                        navigate('/')
+                    }}>Logout</h2>}
             </div>
-
-        </StyledNavBar>
-
+            {open && <LogoutFocus onClick={() => setOpen(false)}>OIe</LogoutFocus>}
+        </Bar>
     )
 }
