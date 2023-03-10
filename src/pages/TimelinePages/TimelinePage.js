88b9fc9f43f7452bb244e9fdce5423c4e820a450
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-03-10 03:22:14 -0300

    Carregamento dinâmico de posts adicionado

--- src/pages/TimelinePages/TimelinePage.js
+++ src/pages/TimelinePages/TimelinePage.js
@@ -1,26 +1,46 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { StyledContainer } from "./styled";
+import CreatePost from "../../components/CreatePost/CreatePost";
 import NavBar from "../../components/NavBar/NavBar";
-import { StyledContainer } from './styled';
-import CreatePost from '../../components/CreatePost/CreatePost';
 import Post from "../../components/Post/Post";
-
-
-
-
+import Context from "../../context/Context";
 
 export default function TimelinePage() {
-       return (
-              <>
+  const [posts, setPosts] = useState([]);
 
-                     <NavBar />
+  const navigate = useNavigate();
+  const { token, setToken } = useContext(Context);
 
-                     <StyledContainer>
-                            <h1>timeline</h1>
-                            <CreatePost />
-                            <Post/>
-                            <Post/>
-                     </StyledContainer>
-              </>
+  async function getAllUsersPosts() {
+    try {
+        const request = await axios.get(`${process.env.REACT_APP_API_URL}/posts`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        setPosts(request.data);
+    } catch (_) {
+        setToken(null);
+        localStorage.removeItem('token');
+        navigate('/');
+    }
+  }
 
+  useEffect(() => {
+    getAllUsersPosts();
+  }, []);
 
-       )
+  return (
+    <>
+      <NavBar />
+      <StyledContainer>
+        <h1>timeline</h1>
+        <CreatePost />
+        {posts && posts.length > 0 && posts.map(post => <Post key={post.id} id={post.id} post={post.post} user_image={post.user_image} username={post.username} post_url={post.post_url}></Post>)}
+      </StyledContainer>
+    </>
+  );
 }
