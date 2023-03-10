71324e768855c1cee34b78dfdba2e18aa9e176be
RubsRafa <rubiarafaela2000@gmail.com>
2023-03-10 17:23:28 -0300

    feat: add avatar image on navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,31 @@
 import { Bar, Logo, LogoutFocus, LogOutBar } from "./styled";
 import { VscChevronDown, VscChevronUp } from 'react-icons/vsc';
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function NavBar() {
     const navigate = useNavigate();
     const [open, setOpen] = useState(false);
+    const [avatar, setAvatar] = useState('https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true');
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        const config = {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          };
+
+        axios.get(`${process.env.REACT_APP_API_URL}/me`, config)
+        .then((res) => {
+            setAvatar(res.data.image)
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+    }, [])
 
     return (
         <Bar>
@@ -23,7 +43,7 @@ export default function NavBar() {
                     color={"#ffffff"}
                     onClick={() => setOpen(false)}
                 />}
-                <img data-test="avatar" onClick={() => setOpen(!open)} src="https://i.kym-cdn.com/photos/images/newsfeed/002/304/580/313.jpg" alt="" />
+                <img data-test="avatar" onClick={() => setOpen(!open)} src={avatar} alt="avatar" />
                 {open &&
                     <LogOutBar data-test="menu">
                         <h2
