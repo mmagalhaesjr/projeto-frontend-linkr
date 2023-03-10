d744981da3c3a185f6609a05c6ec54692b2f157f
franciscojrdev <franciscojunior7222@gmail.com>
2023-03-10 00:27:23 -0300

    feat: some alterations

--- src/pages/HashtagPages/HashtagPage.js
+++ src/pages/HashtagPages/HashtagPage.js
@@ -3,9 +3,32 @@ import NavBar from "../../components/NavBar/NavBar";
 import Post from "../../components/Post/Post";
 import { Container, LeftContent, Title } from "./styled";
 import HashtagBox from "../../components/HashtagBox/HashtagBox";
-
+import { useState,useEffect } from "react";
+import axios from "axios";
 export default function HashtagPage() {
   const { hashtag } = useParams();
+  const [data,setData] = useState([])
+
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_API_URL}/hashtag/${hashtag}`;
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios
+      .get(url, config)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response.message);
+      });
+  }, [token,hashtag]);
+
 
   return (
     <>
